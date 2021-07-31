41f9b818bf1e2584580fa62e0bfd9d5f5a8f587c
Sergey <piolersa85@mail.ru>
2021-07-31 18:26:15 +0200

    Initial Commit

---
+++ TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by Sergey on 31.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
